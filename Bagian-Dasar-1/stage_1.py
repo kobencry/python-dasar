e9a77e6c44c37d09a332950d8cd97e12353e1024
kobencry <ghostbencry@gmail.com>
2022-10-21 23:52:37 +0700

    modified files

--- Bagian-Dasar-1/stage_1.py
+++ Bagian-Dasar-1/stage_1.py
@@ -1,1 +1,82 @@
+# Apa itu Python?
+# Python adalah bahasa pemrograman yang populer. 
+# Itu dibuat oleh Guido van Rossum, dan dirilis pada tahun 1991.
 
+# Ini digunakan untuk:
+# pengembangan web (sisi server),
+# pengembangan perangkat lunak,
+# matematika,
+# skrip sistem.
+
+# Apa yang bisa dilakukan Python?
+# Python dapat digunakan di server untuk membuat aplikasi web.
+# Python dapat digunakan bersama perangkat lunak untuk membuat alur kerja.
+# Python dapat terhubung ke sistem database. Itu juga dapat membaca dan memodifikasi file.
+# Python dapat digunakan untuk menangani data besar dan melakukan matematika yang kompleks.
+# Python dapat digunakan untuk pembuatan prototipe cepat, atau untuk pengembangan perangkat lunak siap produksi.
+
+# Mengapa Python?
+# Python bekerja pada platform yang berbeda (Windows, Mac, Linux, Raspberry Pi, dll).
+# Python memiliki sintaks sederhana yang mirip dengan bahasa Inggris.
+# Python memiliki sintaks yang memungkinkan pengembang untuk menulis program dengan lebih sedikit baris daripada beberapa bahasa pemrograman lainnya.
+# Python berjalan pada sistem interpreter, artinya kode dapat dieksekusi segera setelah ditulis. 
+# Ini berarti bahwa pembuatan prototipe bisa sangat cepat.
+# Python dapat diperlakukan dengan cara prosedural, cara berorientasi objek atau cara fungsional.
+
+# Senang mendengarnya
+# Versi utama terbaru dari Python adalah Python 3, yang akan kita gunakan dalam tutorial ini. 
+# Namun, Python 2, meskipun tidak diperbarui dengan apa pun selain pembaruan keamanan, masih cukup populer.
+# Dalam tutorial ini Python akan ditulis dalam editor teks. 
+# Dimungkinkan untuk menulis Python dalam Lingkungan Pengembangan Terintegrasi, seperti Thonny, Pycharm, Netbeans atau Eclipse yang sangat berguna saat mengelola koleksi file Python yang lebih besar.
+# Sintaks Python dibandingkan dengan bahasa pemrograman lain
+# Python dirancang agar mudah dibaca, dan memiliki beberapa kesamaan dengan bahasa Inggris dengan pengaruh dari matematika.
+# Python menggunakan baris baru untuk menyelesaikan perintah, berbeda dengan bahasa pemrograman lain yang sering menggunakan titik koma atau tanda kurung.
+# Python bergantung pada lekukan, menggunakan spasi, untuk mendefinisikan ruang lingkup; seperti ruang lingkup loop, fungsi dan kelas. 
+# Bahasa pemrograman lain sering menggunakan kurung kurawal untuk tujuan ini.
+
+# Python memiliki sekumpulan kata kunci yang merupakan kata cadangan yang tidak dapat digunakan sebagai 
+# nama variabel, nama fungsi, atau pengidentifikasi lainnya:
+
+#--------------------------------------------
+# Keyword       Deskripsi
+#--------------------------------------------
+# and           operator logis
+# as            untuk membuat alias
+# assert        untuk debugging
+# break         untuk keluar dari looping/putaran
+# class         untuk mendefinisikan kelas
+# continue      untuk melanjutkan ke iterasi looping/putaran berikutnya
+# def           untuk mendefinisikan fungsi
+# del           untuk menghapus objek
+# elif          digunakan dalam pernyataan bersyarat, sama seperti else if
+# else          digunakan dalam pernyataan bersyarat
+# except        digunakan dengan pengecualian, apa yang harus dilakukan ketika pengecualian terjadi
+# False         nilai Boolean, hasil operasi perbandingan
+# finally       digunakan dengan pengecualian, blok kode yang akan dieksekusi tidak peduli apakah ada pengecualian atau tidak
+# for           untuk membuat untuk looping/putaran
+# from          untuk mengimpor bagian tertentu dari modul
+# global        untuk mendeklarasikan variabel global
+# if            membuat pernyataan bersyarat
+# import        untuk mengimpor modul
+# in            untuk memeriksa apakah nilai ada dalam daftar, tupel, dll.
+# is            untuk menguji apakah dua variabel sama
+# lambda        untuk membuat fungsi anonim
+# None          yang mewakili nilai null
+# nonlocal      untuk mendeklarasikan variabel non-lokal
+# not           operator logic/logis
+# or            operator logic/logis
+# pass          pernyataan null, pernyataan yang akan melakukan apa-apa
+# raise         untuk menaikkan pengecualian
+# return        untuk keluar dari fungsi dan mengembalikan nilai
+# True          nilai Boolean, hasil operasi perbandingan
+# try           untuk mencoba...kecuali pernyataan
+# while         untuk membuat looping sementara
+# with          digunakan untuk mempermudah penanganan exception
+# yield         untuk mengakhiri fungsi, mengembalikan generator
+
+# "Peringatan"
+# Jika anda ingin membuat file/nama fungsi jangan menggunakan nama dari sebuah fungsi/modul bawaan dari python
+# karna di python semua adalah objek
+
+# ini adalah komentar
+print("hello world")
