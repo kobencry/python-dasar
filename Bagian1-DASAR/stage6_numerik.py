563d99e4cb1f5f08a30017dd880e749b6d644dc6
kobencry <ghostbencry@gmail.com>
2022-10-25 13:04:35 +0700

    modified files

--- Bagian1-DASAR/stage6_numerik.py
+++ Bagian1-DASAR/stage6_numerik.py
@@ -8,17 +8,17 @@
 x = 10    # int
 y = 10.8  # float
 z = 10j   # complex
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
 
 # int, atau bilangan bulat, adalah bilangan bulat positif atau negatif, dengan panjang tak terhingga.
 x = 9
 y = 9999999999999999999
 z = -999999999999999999
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
 
 # float, atau "angka titik mengambang" adalah bilangan positif atau negatif, 
 # yang mengandung satu atau lebih desimal.
@@ -29,17 +29,17 @@ z = -99.9999999999999999
 sx = 99e9
 sy = 19E4
 sz = -96.6e100
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
-print(sx, type(sx))
-print(sy, type(sy))
-print(sz, type(sz))
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
+print(type(sx), sx)
+print(type(sy), sy)
+print(type(sz), sz)
 
 # complex adalah bilangan yang ditulis dengan huruf "j" sebagai bagian imajiner
 x = 9j
 y = 9+9j
 z = -9j
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
---
+++ Bagian1-DASAR/stage7_casting_type_data.py
@@ -0,0 +1,72 @@
+# mungkin ada saatnya Anda ingin menentukan tipe pada variabel. 
+# Ini bisa dilakukan dengan casting. 
+# python adalah bahasa berorientasi objek, dan karena itu menggunakan kelas 
+# untuk mendefinisikan tipe data, termasuk tipe primitifnya.
+# casting tipe data python dilakukan dengan menggunakan fungsi konstruktor:
+
+# int()
+x = int(1.0)     # float
+y = int("4")     # str
+b1 = int(b'5')   # bytes
+b2 = int(0b101)  # bytes
+oc = int(0o101)  # octal
+hx = int(0x101)  # hexa
+t = int(True)    # boolean
+f = int(False)   # boolean
+
+print(type(x), x)   # 1
+print(type(y), y)   # 4
+print(type(b1), b1) # 5
+print(type(b2), b2) # 5
+print(type(oc), oc) # 65
+print(type(hx), hx) # 257
+print(type(t), t)   # 1
+print(type(f), f)   # 0
+
+# float()
+x = float(1)        # int
+y = float("4")      # str
+b1 = float(b'5')    # bytes
+b2 = float(0b101)   # bytes
+oc = float(0o101)   # octal
+hx = float(0x101)   # hexa
+t = float(True)     # boolean
+f = float(False)    # boolean
+
+print(type(x), x)   # 1.0
+print(type(y), y)   # 4.0
+print(type(b1), b1) # 5.0
+print(type(b2), b2) # 5.0
+print(type(oc), oc) # 65.0
+print(type(hx), hx) # 257.0
+print(type(t), t)   # 1.0
+print(type(f), f)   # 0.0
+
+# str()
+x = str(1)
+y = str(4.0)
+b1 = str(b'5')
+b2 = str(0b101)
+oc = str(0o101)
+hx = str(0x101)
+t = str(True)
+f = str(False)
+
+print(type(x), x)   # 1
+print(type(y), y)   # 4.0
+print(type(b1), b1) # b'5'
+print(type(b2), b2) # 5
+print(type(oc), oc) # 65
+print(type(hx), hx) # 257
+print(type(t), t)   # True
+print(type(f), f)   # False
+
+# Catatan: 
+# Anda tidak dapat mengonversi bilangan kompleks menjadi jenis bilangan lain.
+
+
+# Jika anda ingin mengetahui tentang fungsi int() kunjungi file_name: "Fungsi-Bawaan/fungsi_int.py"
+print(int('1010', 2))  # 10     biner
+print(int('1010', 8))  # 520    octal
+print(int('1010', 10)) # 1010   default
+print(int('1010', 16)) # 4112   hexa
