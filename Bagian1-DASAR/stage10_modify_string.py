3b12f6c1d65f428c45a0ad2998ffe6e98f0dd95c
kobencry <ghostbencry@gmail.com>
2022-10-28 17:26:54 +0700

    modified files

---
+++ Bagian1-DASAR/stage10_modify_string.py
@@ -0,0 +1,24 @@
+# memodifikasi string python
+# python memiliki seperangkat method/metode/fungsi bawaan yang dapat di gunakan pada string
+
+# huruf besar
+# fungsi upper() mengembalikan string dalam huruf besar
+s = "Hello World!"
+print(s.upper())    # HELLO WORLD!
+
+# huruf kecil
+# fungsi lower() mengembalikan string dalam huruf kecil
+s = "HELLO WORLD!"
+print(s.lower())    # hello world!
+
+# menghapus spasi
+# fungsi strip() menghapus karakter spasi sebelum atau setelah string
+s = " <-hello world-> "
+print(s.strip())    #<-hello world->
+
+# mengganti string
+# fungsi replace() mengganti string dengan string lain
+s = "hello world"
+print(s.replace('hello', 'war'))
+
+# Pelajari lebih lanjut tentang method/metode/fungsi bawaan folder_name: "Method-String"
