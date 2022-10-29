5904f6bdb723a3d21f0188d7d4a9619393c51cd9
kobencry <ghostbencry@gmail.com>
2022-10-29 23:43:04 +0700

    modified files

---
+++ Method-String/method_upper.py
@@ -0,0 +1,37 @@
+# -- Method String -- 
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi upper() mengembalikan string dimana semua karakter dalam huruf besar.
+# simbol dan angka diabaikan.
+
+# Syntax
+# string.upper()
+
+# Nilai Parameter
+# Tidak ada nilai parameter
+
+s = "hello world!".upper()
+print(s)    # HELLO WORLD!
+
+s = "99hello world".upper()
+print(s)    # 99HELLO WORLD
+
+print("#hello_world123@gmail.com".upper())  # #HELLO_WORLD123@GMAIL.COM
+
+
+# Ingat: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+s = "hello world!"
+# string melakukan perubahan
+print(s.upper())    # HELLO WORLD!
+# string tidak melakukan perubahan
+print(s)    # hello world!
+
+# string tidak melakukan perubahan
+x = "hello world!"
+x.upper()
+print(x)    # hello world!
