b33d4711d0951a85db511b85f1cef6ce5ee9ee44
kobencry <ghostbencry@gmail.com>
2022-11-03 21:04:45 +0700

    modified files

---
+++ Method-String/method_format.py
@@ -0,0 +1,27 @@
+# -- Method String --
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi format() memformat nilai yang ditentukan dan memasukkannya ke dalam placeholder string.
+# placeholder didefinisikan menggunakan tanda kurung kurawal: {}. 
+# fungsi format() mengembalikan string yang diformat
+
+# Syntax
+# string.format(value1, value2...dst)
+
+# Nilai Parameter
+# parameter             Deskripsi
+# val1, val2..dst       Dibutuhkan. Satu atau lebih nilai yang harus diformat dan dimasukkan ke dalam string.
+#                       nilainya berupa list, nilai yang dipisahkan dengan koma, key=val, atau kombinasi keduanya.
+#                       nilai dapat berupa tipe data apa pun.
+
+data1 = "nama: {x} usia: {y}".format(x="alice", y=23)
+data2 = "nama: {0} usia: {1}".format("carl", 23)
+data3 = "nama: {} usia: {}".format("eliot", 23)
+
+print(data1)
+print(data2)
+print(data3)
+# Pelajari lebih lanjut tentang formatting python di folder_name: "python-formatting"
