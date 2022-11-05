1e603058659cd859c26fa436578e3b137b5a3982
kobencry <ghostbencry@gmail.com>
2022-11-06 01:48:09 +0700

    modified files

---
+++ Method-String/method_isalpha.py
@@ -0,0 +1,27 @@
+# -- Method String --
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi isalpha() mengembalikan nilai boolean True jika semua karakter alfabet (a-z)
+# contoh karakter yang bukan huruf abjad: angka (0-9) (spasi) !@#$%^()?[].;''"" dll
+
+# Syntax
+# string.isalpha()
+
+# Nilai Parameter
+# tidak ada nilai parameter
+
+print("helloworld".isalpha())   # True
+print("hello world".isalpha())  # False
+
+x = "alice123"
+print(x.isalpha())  # False
+
+user = input("masukan alphabet (a-zA-Z): ")
+# apakah variabel user memasukan huruf alphabet (a-zA-Z)?
+if user.isalpha():  # jika kondisi True
+    print("passed")
+else: # jika kondisi False
+    print("failed")
---
+++ Method-String/method_isdecimal.py
@@ -0,0 +1,56 @@
+# -- Method String --
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi isdecimal() mengembalikan boolean True jika semua karakter decimal (0-9)
+# dan bisa juga digunakan pada objek unicode
+
+# Syntax
+# string.isdecimal()
+
+# Nilai Parameter
+# tidak ada nilai parameter
+
+x = "0123456789" # int
+y = "\u0030"     # unicode
+print(x.isdecimal())    # True
+print(y.isdecimal())    # True
+
+
+h = "0xff"
+b = "0b11"
+f = "2.0"
+print(h.isdecimal()) # False
+print(b.isdecimal()) # False
+print(f.isdecimal()) # False
+
+emoji = "🤨" # emoji unicode
+alphabet = "αβγδεζηθικλμνξοπρςστυφχψ" # alphabet unicode
+pecahan = "½"
+
+print(emoji.isdecimal())    # False
+print(alphabet.isdecimal()) # False
+print(pecahan.isdecimal())  # False
+
+print("hello123".isdecimal())   # False
+
+x = "\u0030" # int 0 unicode
+y = "\u0039" # int 9 unicode
+print(x.isdecimal())    # True
+print(y.isdecimal())    # True
+
+print(int(x, base=16))  # 0
+print(int(y, base=16))  # 9
+# jika ingin mempelajari lebih lanjut tentang fungsi-bawaan int() kunjungi folder_name: "Fungsi-Bawaan/fungsi_int.py"
+
+# menghasilkan karakter unicode
+#def kode_unicode(nomor_kode):
+#    return chr(int(nomor_kode.lstrip("U+").zfill(3), base=16))
+#
+#for i in range(30, 81): # range(1, 1000)
+#    print(fr"\u" + str(i).zfill(3), kode_unicode("U+" + str(i)))
+
+# jika ingin mempelajari lebih lanjut tentang formating string kunjungi folder_name: "python-formatting"
+# jika ingin mempelajari lebih lanjut tentang encoding unicode kunjungi folder_name: "python-encoding"
