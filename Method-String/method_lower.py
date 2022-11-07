004d1f693e82ac44c31e00e1f31e482bc891c79e
kobencry <ghostbencry@gmail.com>
2022-11-07 22:11:42 +0700

    modified files

---
+++ Method-String/method_lower.py
@@ -0,0 +1,22 @@
+# -- Method String --
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi lower() mengembalikan string di mana semua karakter adalah huruf kecil.
+# simbol dan Angka diabaikan.
+
+# Syntax
+# string.lower()
+
+# Nilai Parameter
+# tidak ada nilai parameter
+
+x = "HELLO WORLD"
+y = "H3Ll0_w0RlD#1"
+print(x.lower())
+print(y.lower())
+
+print("heLlO_WoRlD@gmail.com".lower())
+print("ABCDEFGHIJKLMNOPQRSTUVWXYZ".lower())
