1c7be1dd089636e2bb322afe7673999dbd54d7ac
kobencry <ghostbencry@gmail.com>
2022-11-10 22:31:21 +0700

    modified files

--- python-operator/operator_aritmatika.py
+++ python-operator/operator_aritmatika.py
@@ -1,2 +1,22 @@
-# operator aritmatika
+# operator aritmatika untuk kasus yang berhubungan dengan hitung menghitung.
+# jenis-jenis operator aritmatika 
+#----------------------------------------------------------
+# Simbol operator           Deskripsi
+#----------------------------------------------------------
+# Penjumlahan (+)           Menjumlahkan 2 buah Operand.
+# Pengurangan (-)           Mengurangkan 2 buah Operand.
+# Perkalian   (*)           Mengalikan 2 buah Operand.
+# Pembagian   (/)           Membagi 2 buah Operand.
+# Modulus     (%)           Menghasilkan sisa bagi dari pembagian 2 bilangan.
+# Pemangkatan(**)           Memangkatkan nilai Operand.
+# Pembagian Bulat (//)      Sama dengan Pembagian hanya saja, angka dibelakang koma akan dihilangkan/dibulatkan.
 
+x = 5
+y = 2
+print(x + y)    # 7
+print(x - y)    # 3
+print(x * y)    # 10
+print(x / y)    # 2.5
+print(x % y)    # 1
+print(x ** y)   # 25
+print(x // y)   # 2
--- python-operator/operator_perbandingan.py
+++ python-operator/operator_perbandingan.py
@@ -1,1 +1,22 @@
-# operator perbandingan
+# operator perbandingan adalah operator yang bertugas untuk membandingkan antar dua Operand. 
+# Jika hasil perbandingan benar, maka akan menghasilkan nilai True, dan sebaliknya jika salah maka akan menghasilkan nilai False.
+# jenis-jenis operator perbandingan
+
+#-------------------------------------------
+# Simbol         Deskripsi
+#-------------------------------------------
+#   >            Lebih besar
+#   <            Lebih kecil
+#  ==            Sama dengan
+#  !=            Tidak sama dengan
+#  >=            Lebih besar sama dengan
+#  <=            Lebih kecil sama dengan
+
+x = 5
+y = 2
+print(x > y)    # True
+print(x < y)    # False
+print(x == y)   # False
+print(x != y)   # True
+print(x >= y)   # True
+print(x <= y)   # False
