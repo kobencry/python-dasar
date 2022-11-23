c5fbd3607519ed10ddfebb4d91c3c1c39612bde1
kobencry <ghostbencry@gmail.com>
2022-11-23 23:47:44 +0700

    modified files

---
+++ Bagian1-DASAR/stage25_set.py
@@ -0,0 +1,7 @@
+# python set
+
+# Set adalah salah satu dari 4 tipe data bawaan di Python yang digunakan untuk
+# menyimpan kumpulan data, 3 lainnya adalah List, Tuple, dan Dictionary,
+# semuanya dengan kualitas dan penggunaan yang berbeda.
+
+# Set adalah kumpulan yang unordered/tidak teratur, unchangeable/tidak dapat berubah, dan unindexed/tidak terindex. 
