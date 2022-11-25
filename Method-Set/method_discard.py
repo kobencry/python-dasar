bf882cb8bce911e145058080ae840fef6983143a
kobencry <ghostbencry@gmail.com>
2022-11-25 22:53:59 +0700

    modified files

--- Method-Set/method_discard.py
+++ Method-Set/method_discard.py
@@ -1,6 +1,6 @@
 # -- Method Set --
 
-# 
+# https://docs.python.org/3/library/stdtypes.html?#frozenset.discard
 
 # fungsi discard() menghapus item yang ditentukan dari set.
 # fungsi ini berbeda dengan fungsi remove(), karena
@@ -14,4 +14,11 @@
 # Parameter                     Deskripsi
 # value                         Dibutuhkan. Item yang akan dicari, dan dihapus.
 
+# menghapus elemen dari set
+x = {'alice', 'carl', 'eliot'}
+x.discard('eliot')
+print(x)    # {'alice', 'carl'}
 
+# tidak memunculkan pesan error runtime
+x.discard('hello')
+print(x)    # {'alice', 'carl'}
