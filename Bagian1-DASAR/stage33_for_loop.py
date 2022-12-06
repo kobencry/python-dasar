bb2117da07ef5e6b600fe824e28685112c94eb41
kobencry <ghostbencry@gmail.com>
2022-12-06 20:07:55 +0700

    modified files

---
+++ Bagian1-DASAR/stage33_for_loop.py
@@ -0,0 +1,7 @@
+# definisi looping/perulangan
+# Secara umum, looping atau perulangan pada Python adalah instruksi kode program 
+# yang dieksekusi berulang kali. 
+# Fungsinya untuk memerintah komputer melakukan sesuatu secara berulang-ulang 
+# dengan jumlah tertentu selama sebuah kondisi yang telah ditentukan masih terpenuhi.
+
+# for loop adalah 
