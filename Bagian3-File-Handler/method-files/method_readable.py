13dfa11a13289952db47c41d4b24117c6c0edf73
kobencry <ghostbencry@gmail.com>
2022-12-27 18:26:26 +0700

    modified files

--- Bagian3-File-Handler/method-files/method_readable.py
+++ Bagian3-File-Handler/method-files/method_readable.py
@@ -9,9 +9,9 @@
 
 # membuka file dengan mode default 'r' (read/baca) atau 't' (text/teks)
 with open('file.txt') as fileku:
-    print(fileku.readable())
+    print("periksa apakah file dapat dibaca:", fileku.readable())
     # Output:
-    # True
+    # periksa apakah file dapat dibaca: True
 # File akan ditutup secara otomatis setelah selesai mengolah
 
 # membaca file gambar
@@ -27,25 +27,12 @@ with open('file_logo_python.png', 'rb') as fileku:
 # Output:
 # b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\xc8'
 
-'''
-Beberapa jenis file yang mungkin tidak bisa dibaca oleh program Python di atas adalah:
+# membuka file dengan mode 'w' (write/tulis)
+with open('file.txt', 'w') as fileku:
+    print("periksa apakah file dapat dibaca:", fileku.readable())
+# File akan ditutup secara otomatis setelah selesai mengolah
+# Output:
+# periksa apakah file dapat dibaca: False
 
-1. File yang tidak ada: 
-* Jika kita mencoba membuka file yang tidak ada, maka akan terjadi error "FileNotFoundError".
-
-2. File yang tidak memiliki izin baca: 
-* Jika file tersebut tidak memiliki izin baca, maka akan terjadi error "PermissionError".
-
-3. File yang dalam format yang tidak dapat dibaca oleh program: 
-* Misalnya, jika kita mencoba membaca file dengan format gambar (seperti JPEG atau PNG) 
-* dengan menggunakan method read(), maka akan terjadi error "UnicodeDecodeError".
-
-4. File yang terproteksi password: 
-* Jika file tersebut terproteksi dengan password, 
-* maka kita perlu memasukkan password terlebih dahulu sebelum dapat membacanya.
-
-Perlu diingat bahwa tergantung pada konteks dan cara menggunakannya, 
-ada kemungkinan bahwa beberapa jenis file di atas mungkin bisa dibaca
-oleh program Python dengan menggunakan cara yang berbeda. 
-Sebagai contoh, untuk membaca file gambar, kita dapat menggunakan library image processing seperti Pillow.
-'''
+# Jadi, method readable() bahwa file hanya dapat dibaca saat dibuka dalam mode “r” 
+# untuk semua mode lainnya, itu tidak dapat dibaca.
