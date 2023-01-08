88bf77a75fbf3bdddbec0621b15a7ffa9ebc5f64
kobencry <ghostbencry@gmail.com>
2023-01-08 20:10:03 +0700

    modified files

--- Bagian3-File-Handler/attribut-files/attribut_encoding.py
+++ Bagian3-File-Handler/attribut-files/attribut_encoding.py
@@ -17,3 +17,14 @@ with open("example.txt") as fileku:
 # jika anda menggunakan os windows
 # Output:
 # jenis encoding: cp1252
+
+# Karena default encoding adalah sistem operasi yang bergantung pada Microsoft Windows, 
+# ini adalah cp1252 tetapi di Linux utf-8. Jadi ketika berhadapan dengan file teks, 
+# itu adalah praktik yang baik untuk menentukan pengkodean karakter. 
+
+# jika anda ingin Outputnya: utf-8 di windows gunakan argumen encoding="utf-8"
+with open("example.txt", encoding="utf-8") as f:
+    print("jenis encoding:", f.encoding)
+# File akan ditutup secara otomatis setelah selesai mengolah
+# Output:
+# jenis encoding: utf-8
