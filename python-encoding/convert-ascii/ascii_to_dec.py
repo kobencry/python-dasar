6d3e62d1bb61cca888e5ffe9eb36879759f805b3
kobencry <ghostbencry@gmail.com>
2023-01-31 20:25:32 +0700

    modified files

--- python-encoding/convert-ascii/ascii_to_dec.py
+++ python-encoding/convert-ascii/ascii_to_dec.py
@@ -1,4 +1,4 @@
-# mengubah karakter ascii ke bit/binary digits(basis 2)
+# mengubah karakter ascii ke decimal(basis 10)
 def convert_ascii(s:str) -> str:
     # Metod ini baru diperkenalkan pada Python 3.7 dan mengembalikan True jika semua karakter dalam string adalah ASCII.
     # menggunakan fungsi-string .isascii() untuk memastikan bahwa string input adalah ASCII.
@@ -9,18 +9,20 @@ def convert_ascii(s:str) -> str:
     # Jika string hanya terdiri dari karakter ASCII, 
     # akan mengembalikan string hasil konversi karakter ASCII menjadi angka desimal, 
     # dengan masing-masing karakter dipisahkan oleh spasi.
-    return " ".join(f"{ord(i):08b}" for i in s)
+    return " ".join(f"{ord(i):d}" for i in s)
 
 print(convert_ascii("Hello world!"))
 # Output:
-# 01001000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 00100001
+# 72 101 108 108 111 32 119 111 114 108 100 33
+# Ini menunjukkan bahwa karakter 'H' memiliki representasi numerik 72 (dalam desimal),
+# karakter 'e' memiliki representasi numerik 101 (dalam desimal), dan seterusnya.
 
 # program ini akan menampilkan pesan error
 print(convert_ascii('hello world✨'))
 # Output:
 # Traceback (most recent call last):
-#   File ".\ascii_to_bin.py", line 20, in <module>
+#   File ".\convert_asciindec.py", line 20, in <module>                                                                                             00d\x00\x00\x00'
 #     print(convert_ascii('hello world✨'))
-#   File ".\ascii_to_bin.py", line 8, in convert_ascii
+#   File ".\convert_asciindec.py", line 8, in convert_ascii
 #     raise ValueError("bukan tabel ascii")
 # ValueError: bukan karakter ascii
