de0a440ce6318bd0634cef8dffb54abb509afab1
kobencry <ghostbencry@gmail.com>
2023-01-31 19:41:29 +0700

    modified files

---
+++ python-encoding/convert-ascii/ascii_to_bin.py
@@ -0,0 +1,17 @@
+# mengubah karakter ascii ke bit(binary digits)
+def convert_ascii(s):
+    # Metod ini baru diperkenalkan pada Python 3.7 dan mengembalikan True jika semua karakter dalam string adalah ASCII.
+    # menggunakan fungsi-string .isascii() untuk memastikan bahwa string input adalah ASCII.
+    # buka folder python-string/metod_string file "metod_isascii.py"
+    if not s.isascii():
+        # Jika string tidak ASCII, maka fungsi akan melemparkan error "ValueError" dengan pesan "bukan tabel ascii".
+        raise ValueError("bukan tabel ascii")
+    # Jika string input ASCII, maka fungsi akan menggunakan expression  generator bersama dengan metod join() 
+    # untuk mengubah string menjadi representasi biner ASCII, 
+    # dan masing-masing karakter akan dikonversi menggunakan fungsi-bawaan ord(). 
+    # Setiap representasi biner akan dipisahkan oleh spasi.
+    return " ".join(f"{ord(i):08b}" for i in s)
+
+print(convert_ascii("Hello world!"))
+# Output:
+# 01001000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 00100001
