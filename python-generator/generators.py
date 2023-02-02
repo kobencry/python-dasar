1b228e194ce5cb42545f6ccdb7cd5f0f7b900cdf
kobencry <ghostbencry@gmail.com>
2023-02-02 21:13:23 +0700

    modified files

---
+++ python-generator/generators.py
@@ -0,0 +1,5 @@
+# Generator adalah suatu objek yang bisa diterima sebagai iterable (seperti list atau tuple),
+# tetapi tidak seperti list atau tuple, generator tidak menyimpan semua elemen dalam memori, 
+# melainkan menghasilkan satu elemen pada satu waktu ketika diterjemahkan.
+# Ini bisa membantu untuk menghemat memori dan lebih efisien dalam hal waktu ketika menangani data besar.
+# Generator dapat dibuat dengan membuat fungsi kustom menggunakan syntax "yield" atau dengan menggunakan generator comprehension.
