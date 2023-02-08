7405de3d9e25ffe9e9b200dce6e4ed566297df32
kobencry <ghostbencry@gmail.com>
2023-02-08 21:34:45 +0700

    modified files

---
+++ python-boolean/stage01_boolean_value.py
@@ -0,0 +1,8 @@
+# Python memiliki tipe data Boolean yang memiliki dua nilai, yaitu True dan False. 
+# Ini berguna ketika kita ingin menentukan suatu kondisi atau membuat logika pada program.
+
+# Nilai Boolean:
+# Dalam pemrograman, Anda sering perlu mengetahui apakah suatu ekspresi adalah True atau False.
+# Anda dapat mengevaluasi ekspresi apa pun dengan Python, dan mendapatkan salah satu dari dua jawaban, True atau False.
+# Saat Anda membandingkan dua nilai, ekspresi dievaluasi dan Python mengembalikan jawaban Boolean:
+
