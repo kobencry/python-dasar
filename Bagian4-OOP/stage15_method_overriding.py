96942db88828218287ef3ccf950cb9a55452c585
kobencry <ghostbencry@gmail.com>
2023-04-18 23:44:41 +0700

    modified files

--- Bagian4-OOP/stage15_method_overriding.py
+++ Bagian4-OOP/stage15_method_overriding.py
@@ -1,1 +1,17 @@
-# 
+# overriding
+
+class A: #superclass
+    # method instance
+    def display(self):
+        return "hello"
+
+class B(A): #subclass
+    # method instance
+    def display(self):
+        return f"{super().display()} world"
+
+# membuat objek
+b = B()
+print(b.display())
+# Output:
+# hello world
