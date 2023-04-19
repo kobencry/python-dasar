a24ce6d41860b6e0bc8c70e0695c68df74541722
kobencry <ghostbencry@gmail.com>
2023-04-20 00:31:48 +0700

    modified files

--- Bagian4-OOP/stage15_method_overriding.py
+++ Bagian4-OOP/stage15_method_overriding.py
@@ -1,22 +1,44 @@
-# overriding ...
-# coming soon
-
-class A: #superclass
+class A:
     # method instance
     def display(self):
         return "hello"
 
-class B(A): #subclass
+class B(A):
     # method instance
     def display(self):
         return f"{super().display()} world"
 
-# membuat objek
+# membuat object
 b = B()
 print(b.display())
-# Output:
-# hello world
 
-class A:
-    def __init__(self) -> None:
-        pass
+class A: #superclass
+    def __init__(self, nama, usia):
+        # variabel instance
+        self.__nama = nama
+        self.__usia = usia
+
+    # method instance
+    def display(self):
+        return f"{self.__nama}, {self.__usia}"
+
+class B(A): #subclass
+    def __init__(self, nama, usia, alamat):
+        # variabel instance
+        super().__init__(nama, usia)
+        self.__alamat = alamat
+
+    # method instance
+    def display(self):
+        return f"{super().display()}, {self.__alamat}"
+
+a = A('alice', 20)
+print(a.display())
+# Output:
+# alice, 20
+
+# membuat object
+b = B('carl', 25, 'bandung')
+print(b.display())
+# Output:
+# carl, 25, bandung
