968ab06fdb9a4e1a20260c19184d5ec6cac1516f
kobencry <ghostbencry@gmail.com>
2023-04-20 20:53:49 +0700

    modified files

--- Bagian4-OOP/stage15_method_overriding.py
+++ Bagian4-OOP/stage15_method_overriding.py
@@ -1,17 +1,59 @@
-class A:
+# Overriding adalah suatu konsep dalam pemrograman berorientasi objek (OOP) 
+# di mana sebuah subclass memiliki fungsi/method dengan nama yang sama seperti method yang didefinisikan di superclassnya.
+# Saat method yang di-override dipanggil pada objek dari subclass, 
+# maka method yang ada di subclass akan dipanggil bukan "method yang ada di superclass".
+
+# Di Python, overriding dapat dilakukan dengan mendefinisikan ulang method di subclass 
+# dengan nama yang sama dengan method yang ada di superclass. 
+# Ketika sebuah objek dari subclass dipanggil dengan method yang di-override, 
+# Python akan mencari method yang sesuai di subclass dan menjalankannya. 
+# Jika method tidak ditemukan di subclass, Python akan mencari di superclassnya.
+
+# Berikut adalah contoh pertama sederhana overriding di Python:
+class A: #superclass
+    # method instance
+    def display(self):
+        return "hello alice"
+
+class B(A): #subclass
+    # method instance
+    def display(self):
+        return "hello carl"
+# membuat object
+a = A()
+print(a.display())
+# Output:
+# hello alice
+
+# membuat object
+b = B()
+print(b.display())
+# Output
+# hello carl 
+
+# Berikut adalah contoh kedua sederhana overriding di Python:
+class A: #superclass
     # method instance
     def display(self):
         return "hello"
 
-class B(A):
+class B(A): #subclass
     # method instance
     def display(self):
         return f"{super().display()} world"
 
 # membuat object
+a = A()
+print(a.display())
+# Output
+# hello
+
 b = B()
 print(b.display())
+# Output
+# hello world
 
+# Berikut adalah contoh sederhana overriding di Python:
 class A: #superclass
     def __init__(self, nama, usia):
         # variabel instance
