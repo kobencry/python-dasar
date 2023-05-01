d2b7f260bfaae2c117ac30f846918467272fc15f
kobencry <ghostbencry@gmail.com>
2023-05-01 14:24:06 +0700

    modified files

---
+++ Bagian4-OOP/stage17_polymorphism.py
@@ -0,0 +1,16 @@
+# Polymorphism pada Python adalah kemampuan sebuah objek untuk memiliki banyak bentuk atau tampilan yang berbeda, 
+# tergantung pada konteksnya. 
+# Polymorphism memungkinkan objek untuk memiliki perilaku yang berbeda saat berinteraksi dengan objek yang berbeda pula.
+
+# Di Python, polymorphism dapat dicapai dengan menggunakan teknik seperti operator overloading, 
+# method overloading, dan method overriding. 
+# Operator overloading memungkinkan operator standar seperti + dan - dapat digunakan pada objek-objek yang kita definisikan sendiri. 
+# Method overloading memungkinkan sebuah kelas memiliki beberapa method dengan nama yang sama, 
+# namun memiliki parameter yang berbeda. 
+# Method overriding memungkinkan kelas turunan untuk menimpa method yang sama pada kelas induk dengan implementasi yang berbeda.
+
+# Polymorphism sangat penting dalam OOP karena memungkinkan kode kita lebih fleksibel dan modular. 
+# Dengan menggunakan polymorphism, kita dapat memanfaatkan abstraksi dan pewarisan(inheritance) pada kelas 
+# untuk membuat kode yang lebih efisien dan mudah di-maintain. 
+# Polymorphism juga dapat meningkatkan reusabilitas kode, 
+# karena objek yang sama dapat memiliki perilaku yang berbeda dalam konteks yang berbeda.
