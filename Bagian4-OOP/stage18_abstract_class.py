aba3dc8e84371b500c27472260d8805bb13c0913
kobencry <ghostbencry@gmail.com>
2023-05-02 20:26:07 +0700

    modified files

---
+++ Bagian4-OOP/stage18_abstract_class.py
@@ -0,0 +1,5 @@
+# Abstract class pada Python adalah sebuah kelas yang tidak dapat diinstansiasi (tidak dapat dibuatkan objeknya) 
+# dan biasanya digunakan sebagai kelas dasar (parent class) 
+# yang memiliki method yang belum diimplementasikan (berisi hanya deklarasi method tanpa implementasi). 
+# Abstract class pada Python digunakan untuk mengimplementasikan konsep abstract method dan interface, 
+# yang merupakan salah satu prinsip dasar dalam pemrograman berorientasi objek.
