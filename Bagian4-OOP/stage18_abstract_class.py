7b3d60aef69925664fcb545d432dfaf32046e59b
kobencry <ghostbencry@gmail.com>
2023-05-02 20:28:01 +0700

    modified files

--- Bagian4-OOP/stage18_abstract_class.py
+++ Bagian4-OOP/stage18_abstract_class.py
@@ -3,3 +3,8 @@
 # yang memiliki method yang belum diimplementasikan (berisi hanya deklarasi method tanpa implementasi). 
 # Abstract class pada Python digunakan untuk mengimplementasikan konsep abstract method dan interface, 
 # yang merupakan salah satu prinsip dasar dalam pemrograman berorientasi objek.
+
+# Dalam Python, untuk membuat sebuah kelas menjadi abstract class, 
+# kita dapat menggunakan modul abc dan mendefinisikan abstract method menggunakan decorator @abstractmethod. 
+# Abstract method adalah sebuah method dalam kelas yang hanya dideklarasikan namun tidak diimplementasikan, 
+# sehingga harus diimplementasikan di kelas turunannya.
