76e2e0bf348b7d84d3afcce977fcb9819be16fd8
kobencry <ghostbencry@gmail.com>
2023-05-25 19:51:36 +0700

    modified files

---
+++ Bagian4-OOP/magic-method/method_abs.py
@@ -0,0 +1,37 @@
+# method khusus __abs__ adalah method khusus dalam Python 
+# yang digunakan untuk mengimplementasikan fungsi bawaan abs() untuk sebuah objek dari sebuah kelas. 
+# method ini memungkinkan Anda mendefinisikan perilaku dari fungsi abs() 
+# ketika diterapkan pada instance kelas yang Anda buat.
+
+# method __abs__ harus mengembalikan nilai absolut dari objek. 
+# method ini dipanggil ketika Anda memanggil fungsi abs() pada sebuah instance dari kelas Anda 
+# atau ketika Anda menggunakan fungsi abs() bawaan secara langsung pada sebuah objek.
+
+# Berikut adalah contoh untuk mengilustrasikan penggunaan method __abs__:
+
+class Nilai:
+    def __init__(self, angka):
+        self.angka = angka
+
+    def __abs__(self):
+        return abs(self.angka)
+
+x = Nilai(-10)
+print(x.__abs__())
+# Output:
+# 10
+
+# menggunakan fungsi bawaan abs()
+print(abs(x))
+# Output:
+# 10
+
+y = Nilai(1+2j)
+print(y.__abs__())
+# Output:
+# 2.23606797749979
+
+# menggunakan fungsi abs()
+print(abs(y))
+# Output:
+# 2.23606797749979
