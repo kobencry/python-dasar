d87ddac7376a5ff7a843b2895b13927112bc55de
kobencry <ghostbencry@gmail.com>
2023-05-27 20:07:59 +0700

    modified files

--- python-operator/operator_bitwise.py
+++ python-operator/operator_bitwise.py
@@ -59,7 +59,7 @@ y = 7
 z = x ^ y
 print(f"x = {x:08b}")   # x = 00000101
 print(f"y = {y:08b}")   # y = 00000111
-print('-'*12, 'XOR')     # ------------ XOR
+print('-'*12, 'XOR')    # ------------ XOR
 print(f"z = {z:08b}")   # z = 00000010
 print(x ^ y)            # 2
 # 128 64 32 16 8 4 2 1
@@ -69,8 +69,8 @@ print(x ^ y)            # 2
 # menggunakan operator NOT ~
 x = 5
 y = 7
-print(f"x = {x:08b}")   # x = 00000101
-print(f"y = {y:08b}")   # y = 00000111
+print(f"x = {x:08b}")    # x = 00000101
+print(f"y = {y:08b}")    # y = 00000111
 print('-'*12, 'NOT')     # ------------ NOT
 print(f"x = {~x:08b}")   # x = -0000110
 print(f"y = {~y:08b}")   # y = -0001000
