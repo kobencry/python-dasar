eed0f46a26c13c8e35e0e024d6c29fd2c6dc0b41
kobencry <ghostbencry@gmail.com>
2023-05-29 23:39:12 +0700

    modified files

---
+++ Bagian4-OOP/magic-method/method_floordiv.py
@@ -0,0 +1,7 @@
+# Magic method __floordiv__ adalah method khusus dalam Python 
+# yang digunakan untuk mengimplementasikan operasi pembagian bulat (floor division) (//) pada objek.
+# method ini memungkinkan objek untuk berperilaku seperti tipe data bawaan yang mendukung operasi pembagian bulat.
+
+Ketika Anda menggunakan operator pembagian bulat (//) antara dua objek, Python akan mencari dan memanggil magic method __floordiv__ pada objek pertama, dan meneruskan objek kedua sebagai argumen. method ini harus mengembalikan hasil pembagian bulat objek tersebut.
+
+Berikut adalah contoh penggunaan magic method __floordiv__ pada suatu kelas:
