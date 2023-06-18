1465b78bbf47552d793a0c82d70915f148cfb410
kobencry <ghostbencry@gmail.com>
2023-06-18 21:18:18 +0700

    modified files

---
+++ Bagian4-OOP/magic-method/method_mod.py
@@ -0,0 +1,11 @@
+# Magic method __mod__ adalah method khusus dalam Python 
+# yang digunakan untuk mengatur perilaku operasi modulo (%) antara dua objek.
+# method ini memungkinkan kita untuk mendefinisikan cara objek dari kelas yang kita buat berperilaku saat dioperasikan dengan operator modulo.
+
+# Ketika operator % digunakan antara dua objek,
+# Python akan mencari dan menggunakan method __mod__ untuk menentukan hasil modulo tersebut.
+# method ini memungkinkan kita mengendalikan bagaimana objek dari kelas yang kita definisikan akan dikenai operasi modulo.
+
+# Syntax:
+# __mod__(self, other)
+
