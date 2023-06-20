1931369ddae870db6fbccfe67cf12cfb5fe011c3
kobencry <ghostbencry@gmail.com>
2023-06-20 19:10:21 +0700

    modified files

--- Bagian4-OOP/magic-method/method_mod.py
+++ Bagian4-OOP/magic-method/method_mod.py
@@ -28,19 +28,19 @@ class Modulus:
 obj1 = Modulus(10)
 obj2 = Modulus(20)
 
-# modulus antara dua objek obj1 dengan obj2
+# modulus antara dua objek obj1 dengan obj2 dari kelas Modulus
 hasil = obj1 % obj2
 print(hasil)
 # Output:
 # 10
 
-# modulus antara obj1 dengan angka
+# modulus antara obj1 dengan bilangan bulat
 hasil = obj1 % 3
 print(hasil)
 # Output:
 # 1
 
-# modulus antara obj2 dengan angka
+# modulus antara obj2 dengan bilangan bulat
 hasil = obj2 % 3
 print(hasil)
 # Output:
