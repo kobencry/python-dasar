16bdbd4bc96a038ee8d2bbdd7b8378d596e16ec9
kobencry <ghostbencry@gmail.com>
2023-06-21 11:23:09 +0700

    modified files

---
+++ Bagian4-OOP/magic-method/method_lt.py
@@ -0,0 +1,10 @@
+# Magic method __lt__ (less than/kurang dari) adalah method khusus dalam Python 
+# yang digunakan untuk mendefinisikan perilaku objek saat dibandingkan dengan operator < (less than/kurang dari).
+# method ini memungkinkan kita untuk membandingkan dua objek dan menentukan apakah objek pertama lebih kecil daripada objek kedua.
+
+# Ketika kita menggunakan operator < antara dua objek, 
+# Python akan mencari dan menggunakan method __lt__ untuk melakukan perbandingan.
+# method ini mengembalikan nilai True jika objek pertama kurang dari objek kedua dalam konteks perbandingan yang didefinisikan,
+# dan mengembalikan nilai False jika tidak.
+
+# Berikut adalah contoh penggunaan method __lt__ dalam sebuah kelas:
