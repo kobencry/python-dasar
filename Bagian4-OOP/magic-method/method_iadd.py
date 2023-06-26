26d8d88b4533a953acaa59a76cfc4ed55fc58f00
kobencry <ghostbencry@gmail.com>
2023-06-26 12:45:32 +0700

    modified files

--- Bagian4-OOP/magic-method/method_iadd.py
+++ Bagian4-OOP/magic-method/method_iadd.py
@@ -5,3 +5,47 @@
 # Definisi:
 # Metode __iadd__() digunakan untuk mengimplementasikan operasi penjumlahan dan penugasan += pada objek. 
 # Metode ini memodifikasi objek saat ini dengan menambahkannya dengan objek yang diberikan.
+
+# Syntax:
+# def __iadd__(self, other):
+#   Implementasi logika penjumlahan dan penugasan
+
+# Parameter:
+# self: Merujuk pada objek saat ini.
+# other: Objek yang akan ditambahkan ke objek saat ini.
+
+# Contoh penggunaan magic method __iadd__():
+class Menghitung:
+    def __init__(self, nilai):
+        self.nilai = nilai
+    
+    def __iadd__(self, other):
+        # Jika objek other juga merupakan objek dari kelas Mahasiswa
+        if isinstance(other, Menghitung):
+            self.nilai += other.nilai
+            return self
+        
+        # Jika objek other bukan merupakan objek Mahasiswa, 
+        # kita asumsikan bahwa other adalah objek lain (tipe data apapun).
+        else:
+            self.nilai += other
+            return self
+
+# membuat objek Menghitung
+x = Menghitung(5)
+y = Menghitung(10)
+
+# penjumlahan dan penugasan antara dua objek x dengan y dari kelas Menghitung
+x += y
+print(x.nilai)
+# Output:
+# 15
+
+# penjumlahan dan penugasan antara dua objek x dengan bilangan bulat
+x += 5
+print(x.nilai)
+# Output:
+# 20
+
+# membuat objek Menghitung
+obj = Menghitung(2)
