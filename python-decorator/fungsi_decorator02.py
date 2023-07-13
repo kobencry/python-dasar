d2fa6f0867a931ed06d35b7b156492b453f4f725
kobencry <ghostbencry@gmail.com>
2023-07-13 19:47:12 +0700

    modified files

---
+++ python-decorator/fungsi_decorator02.py
@@ -0,0 +1,8 @@
+# Nested Functions(fungsi yang didefinisikan di dalam fungsi lain),
+# di sisi lain, adalah fungsi yang didefinisikan di dalam fungsi lain. 
+# Nested Functions digunakan untuk membatasi cakupan fungsi dalam konteks tertentu dan
+# menyediakan cara untuk mengorganisir dan mengelompokkan kode yang terkait secara logis. 
+# Nested Functions tidak secara khusus terkait dengan mengambil fungsi lain sebagai argumen.
+
+# Berikut adalah sintaks umum untuk mendefinisikan fungsi dekorator
+# mengunakan konsep Nested Functions:
