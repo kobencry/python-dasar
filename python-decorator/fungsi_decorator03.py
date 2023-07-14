5a6ccd7f7cffbe8ec85ed15ba9233715bc18d213
kobencry <ghostbencry@gmail.com>
2023-07-14 16:33:38 +0700

    modified files

--- python-decorator/fungsi_decorator03.py
+++ python-decorator/fungsi_decorator03.py
@@ -5,4 +5,21 @@
 
 # Berikut ini adalah contoh kode yang menunjukkan penggunaan dekorator 
 # menggunakan simbol "@" diikuti oleh nama fungsi dekorator di atas definisi fungsi yang akan didekorasi:
+
 # Contoh 1
+def decorator(func):
+    def wrapper():
+        print("-- sebelum --")
+        func()
+        print("-- setelah --")
+    return wrapper
+
+@decorator
+def hello():
+    print("Hello World")
+
+hello()
+# Output:
+# -- sebelum --
+# Hello World
+# -- setelah --
