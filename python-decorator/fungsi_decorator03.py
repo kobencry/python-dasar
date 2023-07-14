f81143d92bd1ca61f6ac293d2ffc970403b391c4
kobencry <ghostbencry@gmail.com>
2023-07-14 16:30:59 +0700

    modified files

---
+++ python-decorator/fungsi_decorator03.py
@@ -0,0 +1,4 @@
+# Dekorator dalam Python didefinisikan menggunakan simbol "@" yang 
+# diikuti oleh nama fungsi dekorator di atas definisi fungsi yang akan didekorasi. 
+# Ini memungkinkan kita untuk menerapkan dekorasi secara langsung pada fungsi 
+# yang akan didekorasi tanpa perlu menetapkan kembali hasil dekorasi ke variabel terpisah.
