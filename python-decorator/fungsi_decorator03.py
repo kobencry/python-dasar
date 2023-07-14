954f43cae0bddeee637c0daa04d6aab81d6080ad
kobencry <ghostbencry@gmail.com>
2023-07-14 16:32:18 +0700

    modified files

--- python-decorator/fungsi_decorator03.py
+++ python-decorator/fungsi_decorator03.py
@@ -2,3 +2,7 @@
 # diikuti oleh nama fungsi dekorator di atas definisi fungsi yang akan didekorasi. 
 # Ini memungkinkan kita untuk menerapkan dekorasi secara langsung pada fungsi 
 # yang akan didekorasi tanpa perlu menetapkan kembali hasil dekorasi ke variabel terpisah.
+
+# Berikut ini adalah contoh kode yang menunjukkan penggunaan dekorator 
+# menggunakan simbol "@" diikuti oleh nama fungsi dekorator di atas definisi fungsi yang akan didekorasi:
+# Contoh 1
