1af39d16d6278337a00757ede413c9675b3b50fc
kobencry <ghostbencry@gmail.com>
2023-07-15 18:23:04 +0700

    modified files

---
+++ python-decorator/fungsi_decorator05.py
@@ -0,0 +1,3 @@
+# Untuk menggunakan dua decorator atau lebih secara bersamaan dengan menggunakan sintaks @,
+# Anda dapat menerapkan decorator-decorator tersebut secara berurutan di atas definisi
+# fungsi yang akan didekorasi. 
