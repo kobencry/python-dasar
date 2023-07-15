927ee43baec646a90789236fc85ffd191cf28ade
kobencry <ghostbencry@gmail.com>
2023-07-15 18:24:46 +0700

    modified files

--- python-decorator/fungsi_decorator05.py
+++ python-decorator/fungsi_decorator05.py
@@ -1,3 +1,19 @@
 # Untuk menggunakan dua decorator atau lebih secara bersamaan dengan menggunakan sintaks @,
 # Anda dapat menerapkan decorator-decorator tersebut secara berurutan di atas definisi
 # fungsi yang akan didekorasi.
+
+# Berikut ini adalah contoh penggunaan dua decorator sekaligus:
+# Contoh 1
+def dekorator1(func):
+    def wrapper(*args, **kwargs):
+        print("Dekorator 1: Sebelum eksekusi fungsi")
+        func(*args, **kwargs)
+        print("Dekorator 1: Setelah eksekusi fungsi")
+    return wrapper
+
+def dekorator2(func):
+    def wrapper(*args, **kwargs):
+        print("Dekorator 2: Sebelum eksekusi fungsi")
+        func(*args, **kwargs)
+        print("Dekorator 2: Setelah eksekusi fungsi")
+    return wrapper
