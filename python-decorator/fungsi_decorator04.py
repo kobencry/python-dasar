b86adf01b449d05e91de73b7e78ed8c263283cbc
kobencry <ghostbencry@gmail.com>
2023-07-15 18:12:28 +0700

    modified files

--- python-decorator/fungsi_decorator04.py
+++ python-decorator/fungsi_decorator04.py
@@ -2,3 +2,24 @@
 # dengan memahami cara kerja decorator dan mengelola nilai kembali yang dihasilkan. 
 
 # Berikut ini adalah contoh penggunaan print dan return secara bersamaan di dalam fungsi decorator:
+
+# contoh 1
+def func_luar(func_lain):
+    def func_dalam(*args, **kwargs):
+        result = func_lain(*args, **kwargs)
+        return result
+    return func_dalam
+
+@func_luar
+def main(x, y):
+    print(x)
+    return y
+
+main(5, 50)
+# Ouput:
+# 5
+
+print(main(100, 200))
+# Output:
+# 100
+# 200
