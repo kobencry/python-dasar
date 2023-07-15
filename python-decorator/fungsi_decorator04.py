1d6880b8c2413bb1b7ec4fcbcc152735d5241267
kobencry <ghostbencry@gmail.com>
2023-07-15 18:11:19 +0700

    modified files

---
+++ python-decorator/fungsi_decorator04.py
@@ -0,0 +1,4 @@
+# Dalam fungsi decorator, Anda dapat menggunakan keyword "print" dan "return" secara bersamaan 
+# dengan memahami cara kerja decorator dan mengelola nilai kembali yang dihasilkan. 
+
+# Berikut ini adalah contoh penggunaan print dan return secara bersamaan di dalam fungsi decorator:
