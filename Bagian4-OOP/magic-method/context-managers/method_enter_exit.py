b19caf1db95dc075aa7f8f07fadd640db70c5f98
kobencry <ghostbencry@gmail.com>
2023-07-17 15:24:03 +0700

    modified files

--- Bagian4-OOP/magic-method/context-managers/method_enter_exit.py
+++ Bagian4-OOP/magic-method/context-managers/method_enter_exit.py
@@ -28,3 +28,17 @@
 #            Jika tidak ada eksepsi, maka exc_value akan bernilai None.
 # traceback: Merupakan traceback (jejak panggilan) exception/pengecualian yang diangkat, jika ada. 
 #            Jika tidak ada eksepsi, maka traceback akan bernilai None.
+
+# Contoh penggunaan __enter__ dan __exit__ dalam blok konteks "with" untuk menutup 
+# file adalah sebagai berikut:
+class FileOpener:
+    def __init__(self, nama_file):
+        self.nama_file = nama_file
+        self.objek_file = None
+    
+    def __enter__(self):
+        self.objek_file = open("demo.txt", 'r')
+        return self.objek_file
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.objek_file.close()
