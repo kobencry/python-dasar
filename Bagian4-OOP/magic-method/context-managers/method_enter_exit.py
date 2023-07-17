f6998b6d382c557f8e8226999a4ff64c8df4cc12
kobencry <ghostbencry@gmail.com>
2023-07-17 15:20:44 +0700

    modified files

--- Bagian4-OOP/magic-method/context-managers/method_enter_exit.py
+++ Bagian4-OOP/magic-method/context-managers/method_enter_exit.py
@@ -3,3 +3,10 @@
 # dengan menggunakan statement "with". 
 # Kedua metode ini bekerja bersama untuk memberikan perilaku yang diinginkan 
 # saat memasuki dan keluar dari blok konteks.
+
+# __enter__ adalah metode yang dieksekusi saat memasuki blok konteks dengan statement "with".
+# Metode ini menginisialisasi konteks dan mengembalikan objek yang akan digunakan dalam blok konteks.
+# __exit__ adalah metode yang dieksekusi saat keluar dari blok konteks dengan statement "with". 
+# Metode ini bertanggung jawab untuk menangani pengecualian(exception) 
+# yang terjadi dalam blok konteks dan melakukan tindakan yang sesuai, 
+# seperti membersihkan sumber daya atau menangani eksepsi tersebut.
