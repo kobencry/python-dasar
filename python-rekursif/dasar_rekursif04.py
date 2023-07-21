2a143c90895f4eb8828e50ad949e389219b32e80
kobencry <ghostbencry@gmail.com>
2023-07-21 17:35:17 +0700

    modified files

--- python-rekursif/dasar_rekursif04.py
+++ python-rekursif/dasar_rekursif04.py
@@ -1,18 +1,20 @@
 # Berikut adalah contoh mencari nilai terbesar dan terkecil menggunakan rekursif
-# def num_max(arr):
-#     nilai_terbesar = arr[0]
-#     print(f"{len(arr)} > 1: {len(arr) > 1}")
-#     if len(arr) > 1:
-#         row = num_max(arr[1:])
-#         print(row)
-#         print(f"{row} > {nilai_terbesar}: {row > nilai_terbesar}")
+def num_max(arr):
+    nilai_terbesar = arr[0]
+    #print(f"{len(arr)} > 1: {len(arr) > 1}")
+    if len(arr) > 1:
+        row = num_max(arr[1:])
+        #print(row)
+        #print(f"{row} > {nilai_terbesar}: {row > nilai_terbesar}")
 
-#         if row > nilai_terbesar:
-#             nilai_terbesar = row
-#     print(f"{arr} -> {nilai_terbesar}")
+        if row > nilai_terbesar:
+            nilai_terbesar = row
+    #print(f"{arr} -> {nilai_terbesar}")
     
-#     return nilai_terbesar
+    return nilai_terbesar
 
-# angka = [0, 20, 100, -10, 3]
+angka = [0, 20, 100, -10, 3]
 
-# print(num_max(angka))
+print(num_max(angka))
+# Output:
+# 100
