b673f834a9411c1241a3ea99eee8462ad409ecf3
kobencry <ghostbencry@gmail.com>
2023-07-25 22:17:08 +0700

    modified files

--- Bagian4-OOP/magic-method/method_dict.py
+++ Bagian4-OOP/magic-method/method_dict.py
@@ -3,4 +3,9 @@
 # Setiap objek dalam Python memiliki __dict__ yang berisi pasangan kunci-nilai(key-vale),
 # di mana kunci adalah nama atribut dan nilainya adalah nilai atribut tersebut.
 
-# Definisi:
+# Deskripsi:
+# Magic method __dict__ digunakan untuk mengakses atribut dari sebuah objek dalam bentuk kamus(dictionary).
+# Kamus yang dihasilkan berisi semua atribut objek beserta nilainya.
+ 
+# Syntax:
+# object.__dict__
