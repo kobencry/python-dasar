65c14de0a6a13a1382c48949ad338a22cb074387
kobencry <ghostbencry@gmail.com>
2023-07-25 22:06:58 +0700

    modified files

---
+++ Bagian4-OOP/magic-method/method_dict.py
@@ -0,0 +1,6 @@
+# Dalam Python, magic method __dict__ adalah salah satu dari banyak magic method di Python. 
+# Magic method ini digunakan untuk mengakses atribut dari sebuah objek dalam bentuk kamus(dictionary). 
+# Setiap objek dalam Python memiliki __dict__ yang berisi pasangan kunci-nilai(key-vale),
+# di mana kunci adalah nama atribut dan nilainya adalah nilai atribut tersebut.
+
+# Definisi:
