db3f22cc525c55f0a95c5237726902756742e66c
kobencry <ghostbencry@gmail.com>
2023-07-27 21:17:57 +0700

    modified files

--- Bagian4-OOP/magic-method/method_oct.py
+++ Bagian4-OOP/magic-method/method_oct.py
@@ -1,4 +1,4 @@
-# Magic method __oct__ adalah method khusus dalam Python 
+# Dalam Python, magic method __oct__ adalah method khusus dalam Python 
 # yang digunakan untuk mengimplementasikan representasi oktal (basis 8) dari sebuah objek.
 # method ini memungkinkan Anda untuk mendefinisikan perilaku objek ketika diubah menjadi 
 # representasi oktal menggunakan fungsi bawaan oct().
@@ -6,7 +6,7 @@
 # method __oct__ harus mengembalikan representasi oktal dari objek dalam bentuk string dengan awalan "0o".
 # method ini dipanggil ketika Anda menggunakan fungsi oct() pada instance objek yang Anda buat.
 
-# Berikut adalah contoh penggunaan method __oct__ dalam sebuah kelas:
+# Berikut adalah contoh penggunaan magic method __oct__ dalam sebuah kelas:
 class NilaiOctal:
     def __init__(self, nilai):
         self.nilai = nilai
@@ -14,6 +14,7 @@ class NilaiOctal:
     def __oct__(self):
         return oct(self.nilai)
 
+# membuat objek NilaiOctal
 x = NilaiOctal(4)
 print(x.__oct__())
 # Output:
@@ -24,6 +25,7 @@ print(oct(4))
 # Output:
 # 0o4
 
+# membuat objek NilaiOctal
 y = NilaiOctal(255)
 print(y.__oct__())
 # Output:
