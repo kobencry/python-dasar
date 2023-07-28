4056102f5c08124eecd4a4b37de56aeaf93d64b8
kobencry <ghostbencry@gmail.com>
2023-07-28 20:50:19 +0700

    modified files

--- Bagian4-OOP/magic-method/method_imul.py
+++ Bagian4-OOP/magic-method/method_imul.py
@@ -1,11 +1,10 @@
-# Dalam Python, magic method __imul__ digunakan untuk mengimplementasikan operasi inplace multiplication(penggandaan dalam tempat) pada objek. 
-# Ketika objek mendukung operasi penggandaan inplace (dengan menggunakan operator *= ), 
-# Python akan mencoba untuk memanggil magic method __imul__ untuk melakukan operasi tersebut.
+# Dalam Python, magic method __imul__ adalah method khusus dalam Python
+# yang digunakan untuk mengimplementasikan operasi perkalian dan penugasan *= pada suatu objek.
+# method ini memungkinkan Anda untuk menentukan perilaku perkalian dan penugasan kustom pada objek Anda.
 
 # Deskripsi:
-# Magic method __imul__ digunakan untuk mengimplementasikan operasi penggandaan dalam tempat pada objek.
-# Saat objek mendukung operasi inplace multiplication (dengan menggunakan operator *= ), 
-# Python akan mencoba untuk memanggil magic method __imul__ pada objek tersebut.
+# method __imul__ digunakan untuk mengimplementasikan operasi perkalian dan penugasan *= pada objek.
+# method ini memodifikasi objek saat ini dengan menambahkanya dengan objek yang diberikan.
 
 # Syntax:
 # def __imul__(self, other):
@@ -14,3 +13,14 @@
 # Parameter:
 # self: Merujuk pada objek saat ini.
 # other: Merupakan objek kedua yang akan digunakan untuk operasi penggandaan.
+# nama parameter "other" bisa diganti dengan nama apapun
+
+class Menghitung:
+    def __init__(self, nilai):
+        self.nilai = nilai
+    
+    def __imul__(self, other):
+        if isinstance(other, Menghitung):
+            return self.nilai * other.nilai
+        else:
+            return self.nilai * other
