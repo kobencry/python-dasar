da0aa30ccfd62268172a9d0d2092414b2bbf01f6
kobencry <ghostbencry@gmail.com>
2023-08-03 15:07:33 +0700

    fix: python magic method __iter__

--- Bagian4-OOP/magic-method/method_iter.py
+++ Bagian4-OOP/magic-method/method_iter.py
@@ -16,3 +16,30 @@
 
 # Parameter:
 # self: Merujuk pada objek saat ini.
+
+# Berikut adalah contoh penggunaan magic method __iter__ dalam sebuah kelas:
+class Range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        # Periksa apakah iterasi sudah selesai
+        if self.start >= self.end:
+            raise StopIteration
+
+        # Ambil nilai saat ini dan naikkan posisi start
+        current_value = self.start
+        self.start += 1
+
+        return current_value
+
+# Penggunaan objek iterasi dalam pernyataan for
+my_range = Range(1, 5)
+for num in my_range:
+    print(num)
+# Output:
+# 
